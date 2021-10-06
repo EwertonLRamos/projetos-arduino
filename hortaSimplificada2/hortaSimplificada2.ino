b8c6b683436085fe77ceaecff4ccdaac7a1c54f0
EwertonLRamos <elres.ewerton@gmail.com>
2021-10-05 23:37:15 -0300

    Projeto da Horta Autônoma Simplificado 2

---
+++ hortaSimplificada2/hortaSimplificada2.ino
@@ -0,0 +1,33 @@
+#define pinSensorA A0
+#define pinSensorB A5
+#define pinSensorD 10
+
+void setup() {
+  pinMode(pinSensorD, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Digital:");
+  
+  if (digitalRead(pinSensorD)) {
+     Serial.print("SEM UMIDADE ");
+  } else {
+     Serial.print("COM UMIDADE ");
+  }
+
+  Serial.print("  Analogico:");
+  Serial.print(analogRead(pinSensorA));
+  Serial.print(" | ");
+  Serial.print(analogRead(pinSensorB)); 
+  Serial.print("  ");
+
+  Serial.print("  Atuador:");
+  if (analogRead(pinSensorA) > 700) {
+     Serial.println("SOLENOIDE LIGADO");
+     //digitalWrite(pinSolenoide, HIGH);
+  } else {
+    Serial.println("SOLENOIDE DESLIGADO");
+     //digitalWrite(pinSolenoide, LOW);
+  }
+}
